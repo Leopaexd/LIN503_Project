6235f5e3916c7f15ca2e170775be70ef75f24c23
Oliver <oliver.glant@gmail.com>
2017-10-24 14:15:30 +0200

    Dictionary class now reports creation and indexing

--- dictionary.py
+++ dictionary.py
@@ -3,9 +3,12 @@
 
 class Dictionary(object):
     dictionary = dict()
+    print("Dictionary created")
 
     def IndexWords(self,post_array):
+        print("Indexing words")
         for post in post_array:
             for word in post:
                 if word not in self.dictionary:
                     self.dictionary[word] = len(self.dictionary)
+        print("Indexing completed")
