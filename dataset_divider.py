c2bbfbb908f5fb8090438b8aa1f5ce81f35e61e5
Oliver <oliver.glant@gmail.com>
2017-10-25 14:44:47 +0200

    Added class with static method for dividing dataset

---
+++ dataset_divider.py
@@ -0,0 +1,16 @@
+# Author: Oliver Glant
+# Divides dataset into training and testing parts at ratio 9:1
+
+class Divider(object):
+
+    @staticmethod
+    def Divide(datafile,lines):
+        label = datafile.strip(".txt")[-1:]
+        file = open(datafile,"r")
+        training = open(('training' + str(label) + ".txt"), "w")
+        for _ in range(int(lines*0.9)): #Writes first 70% of lines into trainingX.txt
+            training.write(file.readline())
+        testing = open(('testing' + str(label) + ".txt"), "w")
+        for line in file:
+            testing.write(line)
+
